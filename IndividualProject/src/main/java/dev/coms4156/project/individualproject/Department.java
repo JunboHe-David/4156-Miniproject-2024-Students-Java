fc2995b0534014f9dc6019b2d9a2f5df6696a29b
Junbo He <jh4737@columbia.edu>
2024-09-12 06:40:22 -0400

    step2: modify MyFileDatabaseTests and append bugs txt.

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Department.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Department.java
@@ -105,8 +105,8 @@ public class Department implements Serializable {
     for (Map.Entry<String, Course> entry : courses.entrySet()) {
       String key = entry.getKey();
       Course value = entry.getValue();
-      result.append(deptCode).append(" ").append(key).append(": ").append(value.toString())
-              .append("\n");
+      result.append(deptCode).append(" ").append(key).append(": ").append(value.toString()).append("; Department Chair: ")
+              .append(departmentChair).append("\n");
     }
     return result.toString();
   }
